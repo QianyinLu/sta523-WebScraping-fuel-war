39ba082ccd75fbc1d3cb834e3f6ed1d127e21f63
Zewen Zhang <zewen.zhang@duke.edu>
2019-10-28 11:30:10 -0400

    get and parse

---
+++ parse_sheetz.R
@@ -0,0 +1,11 @@
+library(dplyr)
+
+sheetz <- readRDS("data/sheetz/get_sheetz.rds")
+
+sheetz[[10]] <- sheetz[[10]][1:7,] %>%
+  mutate(specialDirections = list(NULL))
+
+sheetz <- do.call(rbind, sheetz)
+
+saveRDS(sheetz, file = "data/sheetz/sheetz.rds")
+
