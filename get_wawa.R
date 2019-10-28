e12799061cd6472b3d113ad71fef62ae5d20c326
QianyinLu <qianyin.lu@duke.edu>
2019-10-28 16:39:42 -0400

    parse and get_wawa

---
+++ get_wawa.R
@@ -0,0 +1,15 @@
+library(jsonlite)
+library(tidyverse)
+library(httr)
+base_url <- "https://www2.stat.duke.edu/~sms185/data/fuel/bystore/awaw/awawstore="
+store_urls<-str_c(base_url,sprintf("%05d.json",c(0:1000, 8000:9000)))
+
+wawa_data <- map(store_urls,
+                 function(store_url) {
+                   if (http_error(store_url)==F){
+                     Sys.sleep(rexp(1))
+                     tryCatch(read_json(store_url))
+                   }
+                 }
+)
+saveRDS(wawa_data,file = "data/wawa/get_wawa.rds")
